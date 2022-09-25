14ca606f023927ecc6760e06978e6ea9c60e4292
evanjat2 <ann.jati2@gmail.com>
2022-09-25 13:11:43 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  //throw new Error("Tes doang")
+  res.send("Welcome!");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start()
