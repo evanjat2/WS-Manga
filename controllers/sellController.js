58e0b8c1b59b8d5a387138e406803aeac712fd7a
mustafabaihaqi <mustafabaihaqi@gmail.com>
2022-09-28 21:12:08 +0700

    fix : update sellController.js

--- controllers/sellController.js
+++ controllers/sellController.js
@@ -1,18 +1,16 @@
-import User from "../models/Book.js";
+import Book from "../models/Book.js";
 import { StatusCodes } from "http-status-codes";
-const inputBook = async (req, res) => {
-  const Book = await Book.create(req.body);
+
+const postBook = async (req, res) => {
+  const book = await Book.create(req.body);
   res.status(StatusCodes.CREATED).json({ Book });
 };
-const searchBook = (req, res) =>{
-    
-};
+const getBook = (req, res) => {};
+
+
 const updateBook = (req, res) => {
   res.send("update book");
 };
-const deleteBook =(req, res) =>{
-
-}
-;
-
-export { register, login, updateUser };
+const deleteBook = (req, res) => {};
+const getOneBook = (req, res) => {};
+export { postBook, getBook, updateBook, deleteBook, getOneBook };
