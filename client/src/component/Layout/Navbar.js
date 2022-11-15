a8a170e2f92a371187d1d9113b83f0c140dbecbf
evanjat2 <ann.jati2@gmail.com>
2022-11-15 22:43:48 +0700

    feat(Layout): add navbar

---
+++ client/src/component/Layout/Navbar.js
@@ -0,0 +1,17 @@
+import wsmanga from "../../assets/images/layout/ws-manga.png";
+
+const Navbar = () => {
+  return (
+    <div className="h-[60px] bg-cream fixed w-screen top-0 grid content-center px-[40px] text-inter drop-shadow-lg">
+      <div className="flex justify-between">
+        <img src={wsmanga} className="h-[30px]"></img>
+        <div className="flex justify-between gap-8 font-bold text-blue">
+          <p className="hover:text-orange cursor-pointer">Wishlist</p>
+          <p className="hover:text-orange cursor-pointer">My Cart</p>
+          <p className="hover:text-orange cursor-pointer">Home</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,5 +1,12 @@
 import { useEffect } from "react";
+import { Navbar } from "../component/Layout";
+
 const Landing = () => {
-  return <><div className="">Aaaaaaaaa</div></>;
+  return (
+    <>
+      <Navbar />
+      <div className="h-[200vh] mt-[60px] px-[40px]">Aaaaaaaaa</div>
+    </>
+  );
 };
 export default Landing;
