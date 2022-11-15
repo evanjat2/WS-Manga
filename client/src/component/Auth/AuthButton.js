e58e9d1454402de61b202dd410b21d177fb37e6f
evanjat2 <ann.jati2@gmail.com>
2022-11-15 21:19:22 +0700

    fix(Auth Page): finishing login and signup frontend

--- client/src/component/Auth/AuthButton.js
+++ client/src/component/Auth/AuthButton.js
@@ -1,6 +1,6 @@
 const AuthButton = ({ name }) => {
   return (
-    <div className=" mx-auto w-[140px] h-[30px] relative">
+    <div className=" mx-auto w-[140px] h-[30px] relative font-inter">
       <div className="absolute bg-[#FBAB05] opacity-30 w-full h-full rounded-lg"> </div>
       <div className="absolute text-blue opacity-100 left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">{name}</div>
     </div>
--- client/src/component/Auth/Login.js
+++ client/src/component/Auth/Login.js
@@ -1,10 +1,11 @@
 import Input from "./Input";
 import AuthButton from "./AuthButton";
+
 const Login = () => {
   return (
     <div className="grid min-h-screen content-center px-[5%] sm:px-[8%] md:px-[12%] lg:px-[25%] xl:px-[33%] font-reemkufiink font-bold">
       <form className="px-8 py-8 text-sm 2xl:text-md my-4 flex flex-col gap-4 rounded-[10px]">
-        <p className="text-[#335544] font-notoseriftc text-center text-3xl">
+        <p className="font-notoseriftc text-center text-3xl text-blue">
           Login
         </p>
         <div>
@@ -17,10 +18,10 @@ const Login = () => {
             <Input name="sandi" placeholder="" />
           </label>
         </div>
-        <AuthButton name="Login"/>
-        <div className="flex gap-2 justify-center w-full text-[#000000]">
-          <p className="font-medium">Sign Up</p>
-          <p className="font-semibold text-[#45B69C]">Forgot Password</p>
+        <AuthButton name="Login" />
+        <div className="grid text-center gap-2 justify-center w-full text-[#36447C] opacity-70 font-bold font-inter">
+          <a href="/signup"><p className="cursor-pointer">Sign Up</p></a>
+          <p className="cursor-pointer">Forgot Password</p>
         </div>
       </form>
     </div>
