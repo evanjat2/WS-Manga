70f44d991cb0df95c882784916d3609cb4397f89
mustafabaihaqi <mustafabaihaqi@gmail.com>
2022-11-17 21:56:43 +0700

    feat: update cart page

---
+++ client/src/pages/ProductPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Navbar } from '../component/Layout'
+
+const ProductPage = () => {
+  return (
+    <div className='bg-cream'>
+        ProductPage</div>
+  )
+}
+
+export default ProductPage
