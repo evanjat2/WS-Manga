38edab08dadbce2ec5c6a79af19b8f2503f3a1ce
Adrian463588 <adriansyah2020@mail.ugm.ac.id>
2022-11-17 10:17:07 +0700

    feat : add Penjual Read

--- client/src/component/Profile.js
+++ client/src/component/Profile.js
@@ -1,7 +1,12 @@
 const Profile = () => {
   return (
-    
-    <><div>Profile</div><div>madang geden</div></>
-  )
-}
-export default Profile
+    <>
+      <div>Profile</div>
+      <div>madang geden</div>
+      <div className="text-blue font-bold p-2 bg-[#EDD69E] w-fit cursor-pointer">
+        Logout
+      </div>
+    </>
+  );
+};
+export default Profile;
