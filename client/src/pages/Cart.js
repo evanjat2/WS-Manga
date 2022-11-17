80557d0e4da259e7f8b75bdf3e76a124d2a535fe
mustafabaihaqi <mustafabaihaqi@gmail.com>
2022-11-17 16:57:26 +0700

    feat: Added Cart

---
+++ client/src/pages/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from '../component/Layout'
+const Cart = () => {
+  return (
+    <div>
+        <Navbar />
+        <div className="h-[200vh] mt-[60px] px-[40px]">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Cart
